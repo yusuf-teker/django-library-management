86001d7d6b84045477617006283091ce23ed0fd9
yusuf-teker <79746018+yusuf-teker@users.noreply.github.com>
2021-09-13 15:39:32 +0300

    picture name format

--- library/models/books.py
+++ library/models/books.py
@@ -2,6 +2,20 @@ from django.db import models
 from autoslug import AutoSlugField
 from library.models import CategoriesModel
 from django.contrib.auth.models import User
+import os
+from uuid import uuid4
+def path_and_rename(path):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.bookName:
+            filename = '{}.{}'.format(instance.bookName, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(path, filename)
+    return wrapper
 
 class BooksModel(models.Model):
    
@@ -9,7 +23,8 @@ class BooksModel(models.Model):
     slug = AutoSlugField(populate_from = 'bookName', unique=True)
     bookBlurb = models.TextField()
     categories = models.ManyToManyField(CategoriesModel, related_name='book')
-    coverPicture = models.ImageField(upload_to='book_cover_picture')
+    #coverPicture = models.ImageField(upload_to='book_cover_picture')
+    coverPicture = models.ImageField(upload_to=path_and_rename('book_cover_picture'))
     author = models.CharField(max_length=30)
     numberOfPages = models.IntegerField()
     publicationYear = models.DateField()
