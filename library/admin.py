cba7e24606dd8a212850818a9f263b613a1f7548
yusuf-teker <79746018+yusuf-teker@users.noreply.github.com>
2021-09-14 10:43:11 +0300

    photo

--- library/admin.py
+++ library/admin.py
@@ -2,6 +2,7 @@ from library.models.contact import ContactModel
 from library.models.books import BooksModel
 from library.models.categories import CategoriesModel
 from django.contrib import admin
+from library.models.photo import PhotoModel
 
 # Register your models here.
 
@@ -16,6 +17,13 @@ class BooksAdmin(admin.ModelAdmin):
 admin.site.register(BooksModel)
 admin.site.register(ContactModel
 )
+
+class PhotoAdmin(admin.ModelAdmin):
+    search_fields= ('file','description' , 'uploaded_at')
+    list_displays = {
+        'file','description' , 'uploaded_at'
+    }
+admin.site.register(PhotoModel,PhotoAdmin)
 #class ContactAdmin(admin.ModelAdmin):
 #    list_display = {'name_surname','dateOfCreation'}
 #    search_fields= ('email')
